d9994d8bacf2c9b62e2a952ed8e9a32453371ad1
Gregory <32060161+ggmaleva@users.noreply.github.com>
2024-07-24 18:12:21 +0300

    fix empty provider ref id (#24)
    * fix empty provider ref id
    
    * ignore terminals without provider
    
    ---------
    
    Co-authored-by: ggmaleva <ggmaleva@yandex.ru>

--- src/main/java/dev/vality/dominant/cache/mapper/TerminalsMapper.java
+++ src/main/java/dev/vality/dominant/cache/mapper/TerminalsMapper.java
@@ -2,6 +2,7 @@ package dev.vality.dominant.cache.mapper;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import dev.vality.damsel.domain.DomainObject;
+import dev.vality.damsel.domain.ProviderRef;
 import dev.vality.damsel.domain.Reference;
 import dev.vality.damsel.domain.TerminalObject;
 import dev.vality.damsel.domain_config.Snapshot;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TerminalsMapper {
@@ -21,16 +23,21 @@ public class TerminalsMapper {
         Map<Reference, DomainObject> domainObjectMap = DomainObjectMapExtractor.getDomainObjectMap(cache);
         List<Terminal> terminals = new ArrayList<>();
         for (Map.Entry<Reference, DomainObject> entry : domainObjectMap.entrySet()) {
-            if (entry.getKey().isSetTerminal()) {
+            if (entry.getKey().isSetTerminal() && haveProvider(entry.getValue().getTerminal())) {
                 Terminal terminal = new Terminal();
                 TerminalObject terminalObject = entry.getValue().getTerminal();
                 terminal.setRef(String.valueOf(terminalObject.getRef().getId()));
                 terminal.setName(terminalObject.getData().getName());
                 terminal.setDescription(terminalObject.getData().getDescription());
-                terminal.setProviderRef(String.valueOf(terminalObject.getData().getProviderRef().getId()));
+                ProviderRef providerRef = terminalObject.getData().getProviderRef();
+                terminal.setProviderRef(String.valueOf(providerRef.getId()));
                 terminals.add(terminal);
             }
         }
         return terminals;
+    }
+
+    private static boolean haveProvider(TerminalObject object) {
+        return Objects.nonNull(object.getData().getProviderRef());
     }
 }
