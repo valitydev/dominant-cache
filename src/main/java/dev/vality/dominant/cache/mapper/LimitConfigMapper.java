685d59b0b26c997da57a2ac11c3d1096d8e071cd
vitaxa <vitaxa93gamebox@gmail.com>
2025-02-26 15:28:46 +0300

    fix wrong limit scope format [2]

--- src/main/java/dev/vality/dominant/cache/mapper/LimitConfigMapper.java
+++ src/main/java/dev/vality/dominant/cache/mapper/LimitConfigMapper.java
@@ -5,6 +5,7 @@ import dev.vality.damsel.domain.DomainObject;
 import dev.vality.damsel.domain.LimitConfigObject;
 import dev.vality.damsel.domain.Reference;
 import dev.vality.damsel.domain_config.Snapshot;
+import dev.vality.damsel.limiter.config.LimitScopeEmptyDetails;
 import dev.vality.damsel.limiter.config.LimitScopeType;
 import dev.vality.dominant.cache.mapper.utils.DomainObjectMapExtractor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,7 +29,29 @@ public class LimitConfigMapper {
                         if (scope.getSetField() != null) {
                             validScopes.add(scope);
                         } else {
-                            log.warn("Wrong LimitScopeType. Scope must be set: {}", scope);
+                            if (scope.isSetParty()) {
+                                validScopes.add(LimitScopeType.party(new LimitScopeEmptyDetails()));
+                            } else if (scope.isSetShop()) {
+                                validScopes.add(LimitScopeType.shop(new LimitScopeEmptyDetails()));
+                            } else if (scope.isSetWallet()) {
+                                validScopes.add(LimitScopeType.wallet(new LimitScopeEmptyDetails()));
+                            } else if (scope.isSetIdentity()) {
+                                validScopes.add(LimitScopeType.identity(new LimitScopeEmptyDetails()));
+                            } else if (scope.isSetPaymentTool()) {
+                                validScopes.add(LimitScopeType.payment_tool(new LimitScopeEmptyDetails()));
+                            } else if (scope.isSetProvider()) {
+                                validScopes.add(LimitScopeType.provider(new LimitScopeEmptyDetails()));
+                            } else if (scope.isSetTerminal()) {
+                                validScopes.add(LimitScopeType.terminal(new LimitScopeEmptyDetails()));
+                            } else if (scope.isSetPayerContactEmail()) {
+                                validScopes.add(LimitScopeType.payer_contact_email(new LimitScopeEmptyDetails()));
+                            } else if (scope.isSetSender()) {
+                                validScopes.add(LimitScopeType.sender(new LimitScopeEmptyDetails()));
+                            } else if (scope.isSetReceiver()) {
+                                validScopes.add(LimitScopeType.receiver(new LimitScopeEmptyDetails()));
+                            } else {
+                                log.warn("Wrong LimitScopeType. Scope must be set: {}", scope);
+                            }
                         }
                     }
                     data.setScopes(validScopes);
