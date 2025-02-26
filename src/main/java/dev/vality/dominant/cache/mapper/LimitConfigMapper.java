9382c211d86a51cba5eaa44e7a2a5e238c70583d
vitaxa <vitaxa93gamebox@gmail.com>
2025-02-26 15:20:38 +0300

    fix wrong limit scope format

--- src/main/java/dev/vality/dominant/cache/mapper/LimitConfigMapper.java
+++ src/main/java/dev/vality/dominant/cache/mapper/LimitConfigMapper.java
@@ -5,12 +5,11 @@ import dev.vality.damsel.domain.DomainObject;
 import dev.vality.damsel.domain.LimitConfigObject;
 import dev.vality.damsel.domain.Reference;
 import dev.vality.damsel.domain_config.Snapshot;
+import dev.vality.damsel.limiter.config.LimitScopeType;
 import dev.vality.dominant.cache.mapper.utils.DomainObjectMapExtractor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 public class LimitConfigMapper {
@@ -22,9 +21,21 @@ public class LimitConfigMapper {
             if (entry.getKey().isSetLimitConfig()) {
                 log.debug("Limit config found, key={}, value={}", entry.getKey(), entry.getValue());
                 LimitConfigObject limitConfigObject = entry.getValue().getLimitConfig();
-                dev.vality.damsel.dominant.cache.LimitConfigObject cacheLimitConfig
-                        = new dev.vality.damsel.dominant.cache.LimitConfigObject(
-                                limitConfigObject.ref.id, limitConfigObject.getData());
+                dev.vality.damsel.limiter.config.LimitConfig data = limitConfigObject.getData();
+                if (data != null && data.isSetScopes() && data.getScopes() != null) {
+                    Set<LimitScopeType> validScopes = new HashSet<>();
+                    for (LimitScopeType scope : data.getScopes()) {
+                        if (scope.getSetField() != null) {
+                            validScopes.add(scope);
+                        } else {
+                            log.warn("Wrong LimitScopeType. Scope must be set: {}", scope);
+                        }
+                    }
+                    data.setScopes(validScopes);
+                }
+
+                dev.vality.damsel.dominant.cache.LimitConfigObject cacheLimitConfig =
+                        new dev.vality.damsel.dominant.cache.LimitConfigObject(limitConfigObject.ref.id, data);
                 limitConfigs.add(cacheLimitConfig);
             }
         }
