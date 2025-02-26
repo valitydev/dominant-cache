d5a7066476930b5dc1cb09e7d1fbd812bf314ecb
vitaxa <vitaxa93gamebox@gmail.com>
2025-02-26 12:43:02 +0300

    add limit mapper debug log (#27)

--- src/main/java/dev/vality/dominant/cache/mapper/LimitConfigMapper.java
+++ src/main/java/dev/vality/dominant/cache/mapper/LimitConfigMapper.java
@@ -6,11 +6,13 @@ import dev.vality.damsel.domain.LimitConfigObject;
 import dev.vality.damsel.domain.Reference;
 import dev.vality.damsel.domain_config.Snapshot;
 import dev.vality.dominant.cache.mapper.utils.DomainObjectMapExtractor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 public class LimitConfigMapper {
 
     public static List<dev.vality.damsel.dominant.cache.LimitConfigObject> mapLimits(Cache<String, Snapshot> cache) {
@@ -18,6 +20,7 @@ public class LimitConfigMapper {
         List<dev.vality.damsel.dominant.cache.LimitConfigObject> limitConfigs = new ArrayList<>();
         for (Map.Entry<Reference, DomainObject> entry : domainObjectMap.entrySet()) {
             if (entry.getKey().isSetLimitConfig()) {
+                log.debug("Limit config found, key={}, value={}", entry.getKey(), entry.getValue());
                 LimitConfigObject limitConfigObject = entry.getValue().getLimitConfig();
                 dev.vality.damsel.dominant.cache.LimitConfigObject cacheLimitConfig
                         = new dev.vality.damsel.dominant.cache.LimitConfigObject(
